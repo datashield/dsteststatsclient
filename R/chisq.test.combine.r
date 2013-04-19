e43491211ff0051f0722b9cfc64e3e94bfcb534f
Amadou Gaye <ahmet_gaye@yahoo.fr>
2013-04-19 21:06:53 +0100

    Script ammended

--- R/chisq.test.combine.r
+++ R/chisq.test.combine.r
@@ -25,61 +25,9 @@
 #'
 chisq.test.combine <- function (opals, a, b){
   # use the same approach as in the function table.2d.combine to create a 2d table of counts
-  cally <- call("table.2d", a, b) 
-  output.object <- datashield.aggregate(opals, cally)
+  output.all <- datashield.table.2d(opals, a, b)
   
-  num.sources <- length(output.object)
-  table.valid<-rep(NA,num.sources)
-  
-  for(j in 1:num.sources)
-  {
-    table.valid[j]<-(sum(output.object[[j]])>=0)
-  }
-  
-  tab.final<-as.matrix(output.object[[1]])*0
-  
-  
-  for(j in 1:num.sources)
-  {
-    if(table.valid[j]==1)
-    {
-      tab.final<-tab.final+as.matrix(output.object[[j]])
-    }
-  }
-  
-  
-  total.count<-sum(tab.final)
-  
-  numrows<-dim(tab.final)[1]
-  numcols<-dim(tab.final)[2]
-  
-  table.combination.valid<-1
-  
-  for(j in 1:numrows)
-  {
-    for(m in 2:num.sources)
-    {
-      if(dimnames(output.object[[m-1]])$Var1[j]!= dimnames(output.object[[m]])$Var1[j])
-      {table.combination.valid<-0}
-    }
-  }
-  
-  for(j in 1:numcols)
-  {
-    for(m in 2:num.sources)
-    {
-      if(dimnames(output.object[[m-1]])$Var2[j]!= dimnames(output.object[[m]])$Var2[j])
-      {table.combination.valid<-0}
-    }
-  }
-  
-  col.sum.vect<-rep(1,numrows)
-  row.sum.vect<-rep(1,numcols)
-  
-  col.tots<-t(tab.final)%*%col.sum.vect
-  row.tots<- tab.final%*%row.sum.vect
-  
-  tab.final.counts<-tab.final
+  tab.final.counts <- output.all$final.counts
   cat("\nThe 2D table used for chi-squared test\n\n")
   print(tab.final.counts)
   
